feaa9d795044e27adfa50f6e0d18ed46135a67b0
CO84635 <co84635@mail.harpercollge.edu>
2024-10-24 14:06:34 -0500

    Adding multiple files

---
+++ heatflow.hpp
@@ -0,0 +1,15 @@
+#ifndef HEATFLOW_H
+#define HEATFLOW_H
+
+class HeatFlow {
+public: 
+    HeatFlow(int intitial_temp, int number_of_sections, double constant_k, int sinks_locations[])
+
+
+
+
+}
+
+
+
+#endif // HEATFLOW_H
