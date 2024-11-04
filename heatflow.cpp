d3d47deaa878121c16833053e65795004ae3c9c5
Owen Ceresa <co84635@mail.harpercollege.edu>
2024-11-03 18:49:48 -0600

    lab completed

--- heatflow.cpp
+++ heatflow.cpp
@@ -1,23 +1,53 @@
 #include <iostream>
 #include "heatflow.hpp"
+#include <sstream>
+#include <iomanip>
 
 using namespace std;
 
-HeatFlow::HeatFlow (int initial_temperature, int number_of_sections, double K, std::map<int, float> sources_and_sinks) {
+HeatFlow::HeatFlow (int initial_temperature, int number_of_sections, double k, map<int, double> sources_and_sinks) {
     this->initial_temperature = initial_temperature;
     this->number_of_sections = number_of_sections;
     this->k = k;
     this->sources_and_sinks = sources_and_sinks;
+    for (int i = 0; i < number_of_sections; i++){
+        heatflow_data.push_back(initial_temperature);
+    }
+
+    for (const auto& pair : sources_and_sinks) {
+        heatflow_data[pair.first] = pair.second;
+        known_sources_and_sinks_locations.push_back(pair.first);
+    }
 }
 
 void HeatFlow::tick()
 {
-
+    diffusion_equation();
 }
 
-void HeatFlow::pretty_print()
-{
-    cout << ""
+vector<double> HeatFlow::getHeatFlowData() const{
+    return heatflow_data;
+}
 
-};
+void HeatFlow::pretty_print() {
+    std::stringstream top_and_bottom_brace;
+    std::stringstream middle_data;
+
+    top_and_bottom_brace << corner;
+    for (int i = 0; i < number_of_sections; i++) {
+        top_and_bottom_brace << std::setw(6) << std::setfill('-') << horizontal << corner;
+    }
+    top_and_bottom_brace << std::endl;
+
+    
+    middle_data << vertical; 
+    for (int i = 0; i < number_of_sections; i++) {
+        middle_data << std::setw(6) << std::setfill(' ') << std::fixed << std::setprecision(1) << heatflow_data[i] << vertical; 
+    }
+    middle_data << endl;
+
+    std::cout << top_and_bottom_brace.str();
+    std::cout << middle_data.str();
+    std::cout << top_and_bottom_brace.str(); 
+}
 
--- heatflow.hpp
+++ heatflow.hpp
@@ -1,15 +1,53 @@
 #ifndef HEATFLOW_H
 #define HEATFLOW_H
+#include <vector>
+#include <map>
 
 class HeatFlow {
+private:
+    int initial_temperature;
+    int number_of_sections;
+    double k;
+    std::vector<double> heatflow_data;
+    std::map<int, double> sources_and_sinks;
+    std::vector<int> known_sources_and_sinks_locations;
+    std::string horizontal = "------";
+    std::string vertical = "|";
+    std::string corner = "+";
+    std::string white_space = " ";
+
+    void diffusion_equation(){
+        std::vector<double> new_heatflow_data = heatflow_data;
+        
+        for (int i = 0; i < number_of_sections; i++) {
+
+            bool t_is_source_or_sink = false;
+
+            for (int j : known_sources_and_sinks_locations) {
+                if (i == j){
+                    t_is_source_or_sink = true;
+                    break;
+                }
+            }
+
+            if (!t_is_source_or_sink) 
+            {
+                double t = heatflow_data.at(i);
+                double t_negative_one = (i > 0) ? heatflow_data.at(i - 1) : t;
+                double t_positive_one = (i < number_of_sections - 1) ? heatflow_data.at(i + 1) : t;
+
+                new_heatflow_data[i] = t + k * (t_positive_one - 2 * t + t_negative_one);
+            }
+        }
+
+        heatflow_data = new_heatflow_data;
+    };
+
 public: 
-    HeatFlow(int intitial_temp, int number_of_sections, double constant_k, int sinks_locations[])
-
-
-
-
-}
-
-
+    HeatFlow(int initial_temperature, int number_of_sections, double K, std::map<int, double> sources_and_sinks);
+    std::vector<double> getHeatFlowData() const;
+    void tick();
+    void pretty_print();
+};
 
 #endif // HEATFLOW_H
